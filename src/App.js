e0b61a1c3d61cef2e63ca5b7a9368119afee6607
Anna Esther <93336303+annaestherrr@users.noreply.github.com>
2022-02-03 20:58:27 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Frase extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Consegui uhuuu</h1>
+      </div>
+    );
+  }
+}
+
+export default Frase;
